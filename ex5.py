4b7d17a1df6215b9eda750f5c0de2cd88e5ed4be
samiksha-byte <samikshaganeshmoorthy08@gmail.com>
2026-06-04 20:34:33 +0530

    Create ex5.py

---
+++ ex5.py
@@ -0,0 +1,9 @@
+student = {
+    "name": "Samiksha",
+    "course": "CSE",
+    "city": "Coimbatore",
+    "college": "XYZ College"
+}
+
+for key, value in student.items():
+    print(key, ":", value)
