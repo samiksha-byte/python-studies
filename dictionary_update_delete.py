84c327387287a168a9e55984760833c7497cc924
samiksha-byte <samikshaganeshmoorthy08@gmail.com>
2026-06-04 20:33:45 +0530

    Create dictionary_update_delete.py

---
+++ dictionary_update_delete.py
@@ -0,0 +1,11 @@
+student = {
+    "name": "Samiksha",
+    "course": "CSE",
+    "city": "Mumbai"
+}
+
+student["course"] = "B.tech Computer Science"
+print(student)
+
+del student["city"]
+print(student)
