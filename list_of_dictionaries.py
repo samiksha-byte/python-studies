99330bc953a2f38795946f25c93db9bb4b67af25
samiksha-byte <samikshaganeshmoorthy08@gmail.com>
2026-06-04 20:37:58 +0530

    Create list_of_dictionaries.py

---
+++ list_of_dictionaries.py
@@ -0,0 +1,8 @@
+students = [
+    {"name": "Samiksha", "course": "Btech", "city": "Coimbatore"},
+    {"name": "Zen", "course": "Btech", "city": "Mumbai"},
+    {"name": "Anu", "course": "Btech", "city": "Goa"}
+]
+
+for student in students:
+    print(student)
